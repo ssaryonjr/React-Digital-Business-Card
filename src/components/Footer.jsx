03743bb2d1e62577a16fe1f3131e83c6c5ec9e84
Sam Saryon <ssaryonjr@gmail.com>
2022-08-06 00:13:55 -0700

    project finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons' //make sure to use camel case.
+import { faGithub, faTwitter, faLinkedinIn } from "@fortawesome/free-brands-svg-icons" //Imports brands icons like facebook, twitter, etc.
+
 
 export default function Footer(){
     return (
         <footer>
-        
+            <ul className='footerContact'>
+                <li><a href='#'><FontAwesomeIcon icon={faGithub} /></a></li>
+                <li><a href='#'><FontAwesomeIcon icon={faTwitter} /></a></li>
+                <li><a href='#'><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
+            </ul>
         </footer>
     )
 }
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons' //make sure to use camel case.
-// import { faLinkedin } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons" //Imports brands icons like facebook, twitter, etc.
 
 function MainContent(){
