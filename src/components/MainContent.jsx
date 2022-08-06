79213e53b3fb2335727afb3df7c7fe05009b44f5
Sam Saryon <ssaryonjr@gmail.com>
2022-08-05 23:47:08 -0700

    finally got font awesome working

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons' //make sure to use camel case.
+// import { faLinkedin } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons" //Imports brands icons like facebook, twitter, etc.
 
 function MainContent(){
     return (
@@ -12,11 +13,15 @@ function MainContent(){
                 <h1 className='title'>Sam Saryon Jr.</h1>
                 <h2 className='subTitle'>Full Stack Developer</h2>
                 <a href='#' className='website'>www.samsaryon.com</a>
-                <a href='#' className='email'><FontAwesomeIcon icon={faCoffee} />Email</a>
+                <ul className='topContacts'>
+                    <li><a href='#' className='email'><FontAwesomeIcon icon={faEnvelope} /> Email</a></li>
+                    <li><a href="#" className='linkedin'><FontAwesomeIcon icon={faLinkedin} /> Linkedin</a></li>
+                </ul>
+                
             </div>
             <div className='aboutInfo'>
                 <h5>About</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero totam quis tenetur! Laboriosam commodi numquam, quas, aliquid eos enim nesciunt earum dolorem, in id laborum voluptas! Laboriosam, sapiente obcaecati.</p>
+                <p>I am in love with the fast-paced data driven world that we live in and study it daily. It is my unique combination of passion, drive, and discipline that aids me to develop and deliver world-class software solutions. I am currently open to any new clients or opportunities. Please feel free to contact me through email or Linkedin.</p>
 
                 <h5>Interest</h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero totam quis tenetur! Laboriosam commodi numquam, quas, aliquid eos enim nesciunt earum dolorem, in id laborum voluptas! Laboriosam, sapiente obcaecati.</p>
